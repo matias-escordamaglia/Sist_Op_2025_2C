266bf39544925693d0e27b7de64a033a29bbd52a
matias-escordamaglia <mescordamaglia@frba.utn.edu.ar>
2025-09-09 16:09:32 -0300

    Avances con las queries

---
+++ master/src/queries.c
@@ -0,0 +1,26 @@
+#include "queries.h"
+
+t_list* lista_queries;
+
+static uint32_t contador_qid = 0;
+
+t_query* crear_query(const char* query_path, uint32_t prioridad) {
+
+    if (lista_queries == NULL)
+        lista_queries = list_create();
+
+    t_query* query = malloc(sizeof(t_query));
+	query->query_id = establecer_siguiente_valor_qid();
+    query->prioridad = prioridad;
+	query->program_count = 0;
+	query->estado = READY;
+    query->query_path = string_duplicate(query_path);
+
+    list_add(lista_queries, query);
+
+	return query;
+}
+
+uint32_t establecer_siguiente_valor_qid() {
+    return contador_qid++;
+}
---
+++ master/src/queries.h
@@ -0,0 +1,40 @@
+#ifndef QUERIES_H_
+#define QUERIES_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <commons/log.h>
+#include <commons/string.h>
+#include <commons/config.h>
+
+#include "./utils/utils.h"
+#include "sync.h"
+
+typedef enum {
+    READY,
+    EXEC,
+	EXIT
+} estado_query;
+
+
+typedef struct {
+    uint32_t query_id;
+    uint32_t prioridad;
+    uint32_t program_count;
+    char* query_path;
+    estado_query estado;
+} t_query;
+
+
+t_query* crear_query(const char* query_path, uint32_t prioridad);
+uint32_t establecer_siguiente_valor_qid();
+
+
+
+#endif /* QUERIES_H_ */
