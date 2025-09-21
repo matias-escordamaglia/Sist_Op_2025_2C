253d5fb0858baa540a0b2e8f1e11fdabb739f40b
matias-escordamaglia <mescordamaglia@frba.utn.edu.ar>
2025-09-21 13:10:48 -0300

    Agrego creacion PUNTO MONTAJE

--- storage/src/storage.c
+++ storage/src/storage.c
@@ -60,6 +60,19 @@ void iniciar_estructuras(){
 
     if (FRESH_START == true){ // Iniciamos un FS desde cero
         log_info(logger, "Iniciando seteo de File System... ");
+
+        if (mkdir(PUNTO_MONTAJE, 0777) == -1) {
+            if (errno == EEXIST) {
+                log_info(logger, "Directorio punto de montaje %s ya existe", PUNTO_MONTAJE);
+            } else {
+                log_error(logger, "No se pudo crear el directorio punto de montaje %s. Error: %s", 
+                         PUNTO_MONTAJE, strerror(errno));
+                exit(EXIT_FAILURE);
+            }
+        } else {
+            log_info(logger, "Directorio punto de montaje %s creado correctamente", PUNTO_MONTAJE);
+        }
+
         char* ruta_bitmap = add_seg_ruta(PUNTO_MONTAJE, "/bitmap.bin"); 
         char* ruta_block_hash = add_seg_ruta(PUNTO_MONTAJE, "/blocks_hash_index.config"); 
         char* ruta_f_block = add_seg_ruta(PUNTO_MONTAJE, "/physical_blocks");
@@ -100,9 +113,9 @@ void iniciar_estructuras(){
         }
         log_info(logger, "Inicializando estructuras nuevas...");
         inicializar_super_block_config();
-        //inicializar_dir_physic_block(ruta_f_block); 
-        //inicializar_bitmap(ruta_bitmap);
-        //inicializar_dir_logic_block(ruta_files); 
+        inicializar_dir_physic_block(ruta_f_block); 
+        inicializar_bitmap(ruta_bitmap);
+        inicializar_dir_logic_block(ruta_files); 
 
 
 
