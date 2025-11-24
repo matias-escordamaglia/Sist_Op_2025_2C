5c70f44d7563981906db141bfe8d7b40d120ee95
matias-escordamaglia <mescordamaglia@frba.utn.edu.ar>
2025-11-24 18:26:00 -0300

    Elimino warnings workers

--- worker/src/queryInterpreter.h
+++ worker/src/queryInterpreter.h
@@ -19,7 +19,7 @@ char* const* instrucciones_desde(char* nombre, size_t idx_1based, size_t* out_ca
 char* saltar_blancos(char* p);
 bool empieza_con(char* s, char* kw);
 int ejecutar_create(t_create* c, uint32_t queryid);
-void ejecutarOperacion(t_pedido_master_worker* pedido, char* const* instrucciones, size_t cantidad);
+void ejecutarOperacion(char* const* instrucciones, size_t cantidad);
 bool ejecutar_linea(char* linea, uint32_t query_id);
 void destruir_create(t_create* c);
 bool parsear_create_params(char* params, t_create* out);
@@ -37,6 +37,8 @@ void destruir_tag(t_tag* t);
 int recibir_respuesta_storage(int conexion, t_log* logger);
 int ejecutar_commit(t_create* c,uint32_t queryid);
 int ejecutar_delete(t_create* c, uint32_t queryid);
+void enviar_lectura_a_master(char* file, char* tag, void* contenido, uint32_t tamanio, uint32_t query_id);
+void flush_file_tag_en_memoria(char* file, char* tag, uint32_t id_query);
 void finalizar_query_con_error(t_tipo_aviso_worker_master tipodeerror, int motivo);
 char* storage_error_to_string(int motivo);
 void destruir_write(t_write* w);
