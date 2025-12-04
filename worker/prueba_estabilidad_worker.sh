2e94f635f7f9388abb51b6a9af20547fe6548d41
mescordamaglia <mescordamaglia@frba.utn.edu.ar>
2025-12-04 19:21:36 -0300

    primer script worker estabiliadad

---
+++ worker/prueba_estabilidad_worker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+BIN_WORKER="./bin/worker"
+$CONF_WORKER1="worker_estabilidad_general1.config"
+$CONF_WORKER2="worker_estabilidad_general2.config"
+
+echo "~~~   INICIANDO LAS PRUEBAS DE ESTABILIDAD PARTE 1  ~~~"
+
+
+$BIN_WORKER $CONF_WORKER1 1 &
+echo "-> Lanzado Worker 1"
+
+$BIN_WORKER $CONF_WORKER2 2 &
+echo "-> Lanzado Worker 2"
+
+
+echo ""
+read -p "Presione enter para finalizar los workers..." input
+
+
+echo ""
+echo "  PRUEBA DE ESTABILIDAD FINALIZADA."
