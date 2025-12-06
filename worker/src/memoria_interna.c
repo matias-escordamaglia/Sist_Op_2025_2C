5bd5f82d8deb86220f2127a0bc7d4c40b885a267
matias-escordamaglia <mescordamaglia@frba.utn.edu.ar>
2025-12-06 14:09:31 -0300

    Comento log validacion lazy

--- worker/src/memoria_interna.c
+++ worker/src/memoria_interna.c
@@ -214,7 +214,7 @@ bool rango_valido( t_tabla_paginas* tabla, uint32_t base, uint32_t tam) {
     if (!tabla) return false;
 
     if (tabla->tam_file == 0) {
-        log_warning(logger, "⚠️ Validación Lazy: %s:%s tiene tamaño local 0. Delegando validación al Storage.", 
+        //log_warning(logger, "Validación Lazy: %s:%s tiene tamaño local 0. Delegando validación al Storage.", 
                     tabla->file, tabla->tag);
         return true; 
     }
